601b859f311a91fe399071c1025fbfa913c2876e
Sergio Manzi <smz@smz.it>
2017-03-08 16:36:12 +0100

    Forgot to include an include

---
+++ debug.h
@@ -0,0 +1,19 @@
+// Support stuff for debug...
+#if DEBUG > 0
+  #define DUMP(x)           \
+    Serial.print(" ");      \
+    Serial.print(#x);       \
+    Serial.print(F(" = ")); \
+    Serial.println(x);
+
+  #define DUMP_TM(x)  \
+    DUMP(x.tm_year);  \
+    DUMP(x.tm_mon);   \
+    DUMP(x.tm_mday);  \
+    DUMP(x.tm_hour);  \
+    DUMP(x.tm_min);   \
+    DUMP(x.tm_sec);   \
+    DUMP(x.tm_isdst); \
+    DUMP(x.tm_yday);  \
+    DUMP(x.tm_wday);
+#endif
